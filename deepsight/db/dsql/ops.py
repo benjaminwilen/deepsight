fda31bd9a10952fae1f41dbd936c8739f1d4545a
Benjamin Wilen <benjaminwilen@gmail.com>
2024-08-01 00:55:13 -0500

    dSQL start, dsql will be python, engine will be mostly C++

---
+++ deepsight/db/dsql/ops.py
@@ -0,0 +1,22 @@
+class Query:
+    
+    def __init__(self):
+        pass
+
+    def filter(self):
+        pass
+
+    def where(self):
+        pass
+
+class Select(Query):
+    pass
+
+class Insert(Query):
+    pass
+
+class Update(Query):
+    pass
+
+class Delete(Query):
+    pass
